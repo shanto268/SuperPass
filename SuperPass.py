161bb3e956db5855535a846e7ada6c9433d50caf
LFL-Lab <lfl.lab.bot@gmail.com>
2023-07-18 12:18:27 -0700

    working deployed version

--- SuperPass.py
+++ SuperPass.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 =====================================
 Program : SuperPass/SuperPass.py
@@ -20,21 +19,38 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 # Read the shortcuts and corresponding strings from an environment variable named 'SHORTCUTS'
-shortcuts_json = os.environ.get('SHORTCUTS')
+shortcuts_json = os.getenv('SHORTCUTS')
 
 if not shortcuts_json:
     raise ValueError("Environment variable 'SHORTCUTS' is not set")
 
-shortcuts = json.loads(shortcuts_json)
+# Try to parse the JSON string into a dictionary
+try:
+    shortcuts = json.loads(shortcuts_json)
+except json.JSONDecodeError as e:
+    raise ValueError(f"Failed to parse 'SHORTCUTS' as JSON: {e}")
+
+# Check that the dictionary is not empty
+if not shortcuts:
+    raise ValueError("'SHORTCUTS' is an empty dictionary")
+
+print(f"Loaded shortcuts: {shortcuts}")
 
 def modify_clipboard(defined_string):
+    print(f"Shortcut for string '{defined_string}' activated")
     current_clipboard = pyperclip.paste()
     new_clipboard = defined_string + "\n" + current_clipboard
     pyperclip.copy(new_clipboard)
+    keyboard.write(defined_string)
 
 # Bind the keyboard shortcuts to the modify_clipboard function
 for shortcut, defined_string in shortcuts.items():
-    keyboard.add_hotkey(shortcut, modify_clipboard, args=[defined_string])
+    print(f"Binding shortcut '{shortcut}' to string '{defined_string}'")
+
+    try:
+        keyboard.add_hotkey(shortcut, modify_clipboard, args=[defined_string])
+    except keyboard.InvalidCharacterException as e:
+        print(f"Failed to bind shortcut '{shortcut}': {e}")
 
 # Keep the script running
 while True:
